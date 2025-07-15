91ee32966e55e7c4dfb57a10d7d8911b59702563
SandersonSB <sanderson9777@gmail.com>
2025-07-15 09:28:26 -0300

    Update Datathon.py

--- Datathon.py
+++ Datathon.py
@@ -31,7 +31,7 @@ if "descricao_vaga" not in st.session_state:
 st.markdown("""
     <div style='text-align: center; padding: 30px 0 10px 0;'>
         <img src='https://github.com/SandersonSB/Datathon/blob/main/IA_Gemini_3x0r2u3x0r2u3x0r.png' width='60'/>
-        <h1 style='font-size: 42px; color: #2c3e50; margin-bottom: 10px;'>IA na Decision</h1>
+        <h1 style='font-size: 42px; color:  #FFFFFF; margin-bottom: 10px;'>IA na Decision</h1>
         <h4 style='color: #7f8c8d; font-weight: normal;'>Análise inteligente de currículos com apoio de inteligência artificial</h4>
         <a href="mailto:contato@decisionai.com" style='display: inline-block; margin-top: 10px; font-size: 16px; color: #2980b9; text-decoration: none;'>📧 Fale conosco</a>
         <hr style='border: 1px solid #ddd; margin-top: 20px;'/>
